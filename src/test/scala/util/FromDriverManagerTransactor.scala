c0827539373304a9fe33b1391110fda74e1d19f3
dmedser <dmedser.mephi@gmail.com>
2019-09-08 17:42:25 +0300

    FromDriverManagerTransactor fix

--- src/test/scala/util/FromDriverManagerTransactor.scala
+++ src/test/scala/util/FromDriverManagerTransactor.scala
@@ -1,9 +1,13 @@
 package util
 
 import cats.effect.{Async, ContextShift}
-import doobie.util.transactor.Transactor._
+import doobie.util.transactor.Transactor.Aux
+import doobie.util.transactor._
 
 object FromDriverManagerTransactor {
-  def apply[F[_] : Async : ContextShift](driver: String, url: String, user: String, pass: String): F[Aux[F, Unit]] =
-    Async[F].delay(fromDriverManager(driver, url, user, pass))
+  def apply[F[_]](driver: String, url: String, user: String, pass: String)(
+    implicit F: Async[F],
+    cs: ContextShift[F]
+  ): F[Aux[F, Unit]] =
+    F.delay(Transactor.fromDriverManager(driver, url, user, pass))
 }
